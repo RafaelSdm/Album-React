570d476e4da4ab3cd105f264bb938b235a17c71f
RafaelSdm <rafaeldamasceno2000@gmail.com>
2022-02-03 16:14:59 -0300

    album done

--- react-album/src/pages/Album.tsx
+++ react-album/src/pages/Album.tsx
@@ -1,7 +1,65 @@
+import {useEffect, useState} from 'react'
+import {useParams, useNavigate} from 'react-router-dom'
+import {api} from '../api'
+import {Photo} from '../types/Photo'
+import {Album as AlbumType} from '../types/Album'
+import {PhotoItem} from '../components/PhotoItem'
+
 export const Album = () =>{
+
+    const params = useParams();
+    const navigate = useNavigate();
+
+    const [loading, setLoading] = useState(false);
+    const [list,setList] = useState<Photo[]>([]);
+    const[albumInfo,setAlbumInfo] = useState<AlbumType>({id:0, title:'', userId:0})
+
+
+    useEffect(() =>{
+        if(params.id){
+            loadPhotos(params.id);
+            loadAlbumInfo(params.id);
+        }
+    })
+
+
+    const loadPhotos = async (id: string) =>{
+        setLoading(true);
+        const photos = await api.getPhotosFromAlbum(id);
+        setList(photos)
+        setLoading(false)
+    }
+
+    const loadAlbumInfo =async (id:string) => {
+        const albumInfo = await api.getAlbum(id);
+        setAlbumInfo(albumInfo);
+        
+    }
+
+    const handleBackButton = () =>{
+        navigate(-1);
+    }
+
+
     return(
         <div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, vel animi et laudantium a ipsa! Quidem amet illo voluptates quo excepturi ea, quas a autem enim qui ut, praesentium quaerat.
+
+            <button onClick={handleBackButton}>Voltar</button>
+            <br />
+
+            {loading && "Carregando..."}
+
+            <h1>{albumInfo.title}</h1>
+
+            {list.map((item, index) =>(
+                <PhotoItem
+
+                    key={index}
+                    data={item}
+                
+                />
+            ))}
+         
         </div>
     )
 }
--- react-album/src/routes/MainRoutes.tsx
+++ react-album/src/routes/MainRoutes.tsx
@@ -1,11 +1,13 @@
 import {useRoutes} from 'react-router-dom'
 
 import { Home } from '../pages/Home'
+import {Album} from '../pages/Album'
 
 export const MainRoutes = () =>{
     
         return useRoutes([
-            {path: '/', element: <Home/>}
+            {path: '/', element: <Home/>},
+            {path:'/album/:id', element: <Album/>}
         ])
     
 }
