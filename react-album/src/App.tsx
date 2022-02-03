55df9b9c8db28f1dc6e9c577bb57376183bff1bf
RafaelSdm <rafaeldamasceno2000@gmail.com>
2022-02-03 13:04:14 -0300

    commit full course

---
+++ react-album/src/App.tsx
@@ -0,0 +1,12 @@
+
+const App = () =>{
+  return(
+    <div>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa hic dolore omnis laudantium corporis suscipit unde eum, ducimus veritatis officiis laboriosam aspernatur provident dolorem nihil? Ratione aliquam illo nihil consectetur.</p>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus laborum nemo necessitatibus enim ullam eveniet odit sequi incidunt saepe nihil ipsa itaque veniam, quibusdam, dolore, velit ducimus pariatur alias illum!</p>
+    </div>
+  )
+}
+
+
+export default App;
